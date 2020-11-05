febb2bb025ecd53cb403dae043b791d7253b9872
kosmo_02 <kosmo_02@DESKTOP-QPCUHSN>
2020-11-05 18:29:02 +0900

    레이아웃

--- Airport/src/Airport/pg1_2pg2_2.java
+++ Airport/src/Airport/pg1_2pg2_2.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
 
 public class pg1_2pg2_2 extends JFrame {
 
@@ -35,8 +36,20 @@ public class pg1_2pg2_2 extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton_1 = new JButton("홈으로 돌아가기");
+		btnNewButton_1.setBounds(301, 10, 121, 23);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton = new JButton("선택 완료");
+		btnNewButton.setBounds(164, 228, 97, 23);
+		contentPane.add(btnNewButton);
+	}
+	
+	public class button_array{
+		public static void main(String[] args)
 	}
 
 }
